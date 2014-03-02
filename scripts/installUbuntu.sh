b8ee4be9abadf1d5681b01d11cfb6689b91f87bb
hidabe <hidabe@hidabe-W130EW.(none)>
2014-03-02 22:38:58 +0100

    Remove any scripts

--- scripts/installUbuntu.sh
+++
@@ -1,5 +0,0 @@
-# Installing imagick: http://devilsworkshop.org/tutorial/php54-imagemagick-pecl-installation/61444/
-apt-get install php-pear php5-dev imagemagick php5-imagick
-apt-get install libmagickwand-dev # libmagick9-dev
-pecl install imagick # pear config-set preferred_state beta
-# Add to PHP.ini: extension=imagick.so
