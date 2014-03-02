14f41ee26f900f4e7923a8ba6bca66432857df7a
Sergio Yamuza <syamuza@sopinet.com>
2014-03-03 00:29:14 +0100

    users login enabled

--- src/Trazeo/FrontBundle/Controller/PublicController.php
+++ src/Trazeo/FrontBundle/Controller/PublicController.php
@@ -5,6 +5,7 @@ namespace Trazeo\FrontBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/")
@@ -18,5 +19,11 @@ class PublicController extends Controller
     public function indexAction()
     {
         return $this->render('TrazeoFrontBundle:Public:home.html.twig');
+    }
+    
+    public function getTokenAction()
+    {
+    	return new Response($this->container->get('form.csrf_provider')
+    			->generateCsrfToken('authenticate'));
     }
 }
