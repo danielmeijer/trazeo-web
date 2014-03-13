5f52a15117a83306db8bb166d2ceaed0c8f21311
hidabe <hidabe@hidabe-W130EW>
2014-03-13 13:20:19 +0100

    Hecha estructura de sliders

--- src/Trazeo/FrontBundle/Controller/PublicController.php
+++ src/Trazeo/FrontBundle/Controller/PublicController.php
@@ -18,7 +18,35 @@ class PublicController extends Controller
 	 */
     public function indexAction()
     {
-        return $this->render('TrazeoFrontBundle:Public:home.html.twig');
+    	$banners = array();
+    	
+    	$banners[0]['url'] = "http://static.trazeo.es/banner/100/acerapeatonal.jpg";
+    	$banners[0]['title'] = "Acera Peatonal";
+    	$banners[0]['desc'] = "Entre todos hacemos calle.";
+    	
+    	$banners[1]['url'] = "http://static.trazeo.es/banner/100/elportillo.jpg";
+    	$banners[1]['title'] = "El Portillo";
+    	$banners[1]['desc'] = "Imprescindible, oportuno y necesario Trazeo y sus caminos escolares seguros.";
+
+    	$banners[2]['url'] = "http://static.trazeo.es/banner/100/quaip.png";
+    	$banners[2]['title'] = "QuaIP";
+    	$banners[2]['desc'] = "Imprescindible, oportuno y necesario Trazeo y sus caminos escolares seguros.";
+
+    	$banners[3]['url'] = "http://static.trazeo.es/banner/100/suspasitos.png";
+    	$banners[3]['title'] = "SusPasitos";
+    	$banners[3]['desc'] = "Pasito a pasito; por una ciudad limpia de coches y peligros para nuestros hijos.";    	
+    	
+    	/*
+    	$banner[3]['url'] = "http://static.trazeo.es/banner/100/quaip.png";
+    	$banner[3]['title'] = "DK Diseño kreativo";
+    	$banner[3]['desc'] = "Imprescindible, oportuno y necesario Trazeo y sus caminos escolares seguros.";
+    	*/
+    	
+    	
+        return array(
+        	'banners' => $banners
+        	);
+        //$this->render('TrazeoFrontBundle:Public:home.html.twig');
     }
     
     /**
