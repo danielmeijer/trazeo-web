2ea903070f33a8c6dc2d4325a6136957e9f0aa99
Sergio Yamuza <syamuza@sopinet.com>
2014-03-13 10:25:16 +0100

    ddbb queries through DQL

--- src/Trazeo/FrontBundle/Controller/PanelController.php
+++ src/Trazeo/FrontBundle/Controller/PanelController.php
@@ -18,24 +18,13 @@ class PanelController extends Controller
     public function indexAction()
     {
     	$em = $this->getDoctrine()->getManager();
-    	$fosUser = $this->container->get('security.context')->getToken()->getUser();
-    	$fosUsername = $fosUser->getUsername();
-    	$fosUserEmail = $fosUser->getEmail();
-    	
-    	$user = $em->getRepository('TrazeoBaseBundle:UserExtend')->findByNick($fosUsername);
-    	
-    	$queryGroups = $em->createQuery("SELECT g FROM TrazeoBaseBundle:Groups g JOIN g.userextendgroups u WHERE u.id = u.id");
-    	$groups = $queryGroups->getResult();
-    	
-    	$queryChildren = $em->createQuery("SELECT c FROM TrazeoBaseBundle:Children c JOIN c.userextendchildren u WHERE u.id = u.id");
-    	$children = $queryChildren->getResult();
-    	
-    	$queryRoutes = $em->createQuery("SELECT r FROM TrazeoBaseBundle:Routes r JOIN r.admin u WHERE u.id = u.id");
-    	$routes = $queryRoutes->getResult();
-    	
- 
-    	
-    	
+    	$fosUser = $this->container->get('security.context')->getToken()->getUser();	
+    	$user = $em->getRepository('TrazeoBaseBundle:UserExtend')->findOneByUser($fosUser);
+    
+    	$groups = $user->getGroups();
+    	$children = $user->getChildren();
+    	$routes = $user->getAdminRoutes();
+
 	    	return $this->render('TrazeoFrontBundle:Panel:home.html.twig',array(
 	    			'user' => $user, 'groups' => $groups, 'children' => $children, 'routes' => $routes));  
 	}
