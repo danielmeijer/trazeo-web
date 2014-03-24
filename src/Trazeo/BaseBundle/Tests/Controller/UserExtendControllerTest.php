a3641900f1ed48dce983f16420e4afbb0975acfa
Sergio Yamuza <syamuza@sopinet.com>
2014-03-24 10:21:42 +0100

    new panel template design

--- src/Trazeo/BaseBundle/Tests/Controller/UserExtendControllerTest.php
+++ src/Trazeo/BaseBundle/Tests/Controller/UserExtendControllerTest.php
@@ -13,8 +13,8 @@ class UserExtendControllerTest extends WebTestCase
         $client = static::createClient();
 
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/panel/userextend/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /panel/userextend/");
+        $crawler = $client->request('GET', '/userextend/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /userextend/");
         $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
 
         // Fill in the form and submit it
