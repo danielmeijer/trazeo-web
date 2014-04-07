635822b27d1f99d616ca0f7d7ed354cd8f002fd9
Luismi <llopez@sopinet.com>
2014-04-07 10:06:26 +0200

    Add votes on timeline-bundle

--- src/Trazeo/BaseBundle/Entity/Comment.php
+++ src/Trazeo/BaseBundle/Entity/Comment.php
@@ -5,12 +5,15 @@ namespace Trazeo\BaseBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use FOS\CommentBundle\Model\VotableCommentInterface;
 
 /**
  * @ORM\Entity
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  */
-class Comment extends BaseComment
+class Comment extends BaseComment implements SignedCommentInterface, VotableCommentInterface
 {
     /**
      * @ORM\Id
@@ -26,5 +29,71 @@ class Comment extends BaseComment
      * @ORM\ManyToOne(targetEntity="Trazeo\BaseBundle\Entity\Thread")
      */
     protected $thread;
+    
+    /**
+     * Author of the comment
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Sonata\UserBundle\Entity\User")
+     * @var User
+     */
+    protected $author;
+    
+    /**
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    protected $score = 0;
+    
+    /**
+     * Sets the score of the comment.
+     *
+     * @param integer $score
+     */
+    public function setScore($score)
+    {
+    	$this->score = $score;
+    }
+    
+    /**
+     * Returns the current score of the comment.
+     *
+     * @return integer
+     */
+    public function getScore()
+    {
+    	return $this->score;
+    }
+    
+    /**
+     * Increments the comment score by the provided
+     * value.
+     *
+     * @param integer value
+     *
+     * @return integer The new comment score
+     */
+    public function incrementScore($by = 1)
+    {
+    	$this->score += $by;
+    }
+    
+    public function setAuthor(UserInterface $author)
+    {
+    	$this->author = $author;
+    }
+    
+    public function getAuthor()
+    {
+    	return $this->author;
+    }
+    
+    public function getAuthorName()
+    {
+    	if (null === $this->getAuthor()) {
+    		return 'Anonymous';
+    	}
+    
+    	return $this->getAuthor()->getUsername();
+    }
 
 }
