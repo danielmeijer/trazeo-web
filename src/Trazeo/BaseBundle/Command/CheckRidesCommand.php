85b20e533cf226305fb93800d6c08eb3a1108616
Luismi <llopez@sopinet.com>
2014-05-07 23:22:32 +0200

    Checkridescommand finish

--- src/Trazeo/BaseBundle/Command/CheckRidesCommand.php
+++ src/Trazeo/BaseBundle/Command/CheckRidesCommand.php
@@ -7,8 +7,10 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Application\Sonata\UserBundle\Entity\User;
+use Trazeo\BaseBundle\Entity\EEvent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RequestContext;
+use Sopinet\Bundle\SimplePointBundle\ORM\Type\SimplePoint;
 
 class CheckRidesCommand extends ContainerAwareCommand
 {
@@ -34,7 +36,7 @@ class CheckRidesCommand extends ContainerAwareCommand
     	$lastEvent = $events[0];
     	$lastEventDate = $lastEvent->getCreatedAt();
 
-    	$output->writeln('<info>Revisando si el último evento creado fue hace más de una hora....</info>');
+    	$output->writeln('<info>Revisando si el último evento creado fue hace más de 5min....</info>');
     	$output->writeln('<info>Eliminando...</info>');
     	
     	// Current datetime
@@ -43,16 +45,38 @@ class CheckRidesCommand extends ContainerAwareCommand
     	$result = $lastEventDate->diff($dateCurrent);
     	// Nº de minutos
  		$minutes = $result->i;
+ 		
+ 		$rideId = $lastEvent->getRide()->getId();
+ 		$ride = $em->getRepository("TrazeoBaseBundle:ERide")->find($rideId);
+ 		$rideGroup = $ride->getGroup();
+ 		
  		//ldd($minutes);
- 		if($minutes > 5){
- 			
- 			$rideId = $lastEvent->getRide()->getId(); 			
- 			$ride = $em->getRepository("TrazeoBaseBundle:ERide")->find($rideId);
- 			$rideGroup = $ride->getGroup();
+ 		if($minutes >= 5 && $rideGroup != null){
  			
  			// Detener el paseo del grupo
  			$rideGroup->setHasRide(0);
- 			$em->persist($rideGroup); 			
+ 			
+ 			$em->persist($rideGroup);
+ 			
+ 			//Cálculo del tiempo transcurrido en el paseo
+ 			$inicio = $ride->getCreatedAt();
+ 			$fin = new \DateTime();
+ 			
+ 			$diff = $inicio->diff($fin);
+ 			$duration = $diff->h." horas, ".$diff->i." minutos y ".$diff->s." segundos";
+ 			
+ 			$ride->setDuration($duration);
+ 			$ride->setGroupid($rideGroup->getId());
+ 			$ride->setGroup(null);
+ 			$em->persist($ride);
+ 			
+ 			$event = new EEvent();
+ 			$event->setRide($ride);
+ 			$event->setAction("finish");
+ 			$event->setData("");
+ 			//$event->setLocation(new SimplePoint($latitude, $longitude));
+ 			$em->persist($event); 			
+ 						
  			$em->flush();
  			
  			$output->writeln('<fg=yellow>Paseo del grupo ' . $rideGroup->getName() . ' detenido</fg=yellow>');
