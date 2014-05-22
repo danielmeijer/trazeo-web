df2c34be24b0ca8ab269e7588fa9b4c8d58948c6
alejandro-arrabal <alejandro.arrabal.hidalgo1986@gmail.com>
2014-05-22 20:18:00 +0200

    suggestion script finish integration

--- src/Trazeo/BaseBundle/Entity/ESuggestion.php
+++ src/Trazeo/BaseBundle/Entity/ESuggestion.php
@@ -29,6 +29,20 @@ class ESuggestion
 	protected $rule;
 	
 	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="text", type="string", length=255)
+	 */
+	protected $text;
+	
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="element", type="string", length=255)
+	 */
+	protected $element;
+	
+	/**
 	 * @var integer
 	 *
 	 * @ORM\Column(name="order", type="integer", length=10)
@@ -91,5 +105,53 @@ class ESuggestion
     public function getOrder()
     {
         return $this->order;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return ESuggestion
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set element
+     *
+     * @param string $element
+     *
+     * @return ESuggestion
+     */
+    public function setElement($element)
+    {
+        $this->element = $element;
+
+        return $this;
+    }
+
+    /**
+     * Get element
+     *
+     * @return string 
+     */
+    public function getElement()
+    {
+        return $this->element;
     }
 }
