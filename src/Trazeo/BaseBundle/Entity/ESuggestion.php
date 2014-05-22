7252fcaf406f632419d764899d89d8d1bc65ddb4
hidabe <fhidalgo@sopinet.com>
2014-05-22 21:29:21 +0200

    Fixed ESuggestion methods

--- src/Trazeo/BaseBundle/Entity/ESuggestion.php
+++ src/Trazeo/BaseBundle/Entity/ESuggestion.php
@@ -47,19 +47,111 @@ class ESuggestion
 	 *
 	 * @ORM\Column(name="order", type="integer", length=10)
 	 */
-	protected $order;	
-	
-	/**
-	 * @var string
-	 *
-	 * @ORM\Column(name="element", type="string", length=255)
-	 */	
-	protected $element;
+	protected $order;
 
-	/**
-	 * @var string
-	 * TODO: TRADUCIR
-	 * @ORM\Column(name="text", type="string", length=500)
-	 */
-	protected $text;
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set rule
+     *
+     * @param string $rule
+     *
+     * @return ESuggestion
+     */
+    public function setRule($rule)
+    {
+        $this->rule = $rule;
+
+        return $this;
+    }
+
+    /**
+     * Get rule
+     *
+     * @return string 
+     */
+    public function getRule()
+    {
+        return $this->rule;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return ESuggestion
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set element
+     *
+     * @param string $element
+     *
+     * @return ESuggestion
+     */
+    public function setElement($element)
+    {
+        $this->element = $element;
+
+        return $this;
+    }
+
+    /**
+     * Get element
+     *
+     * @return string 
+     */
+    public function getElement()
+    {
+        return $this->element;
+    }
+
+    /**
+     * Set order
+     *
+     * @param integer $order
+     *
+     * @return ESuggestion
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+
+        return $this;
+    }
+
+    /**
+     * Get order
+     *
+     * @return integer 
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
 }
