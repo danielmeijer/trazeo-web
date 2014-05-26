20fdcaf3c39cd2274e779040ab2e642560226b52
sopinet <sopinet@sopinet-desktop.(none)>
2014-05-26 14:09:08 +0200

    added service for get cities data

--- src/Trazeo/BaseBundle/Service/Helper.php
+++ src/Trazeo/BaseBundle/Service/Helper.php
@@ -27,16 +27,43 @@ class Helper {
 		
 		$query = $reJJ->createQueryBuilder('a');
 		
-		$query->select("a.id, a.nameUtf8")
+		if(!$objects)$param_return="a.id, a.nameUtf8";
+		else $param_return="a";
+
+		$query->select($param_return)
 		->where('a.nameUtf8 LIKE :name')
 		->setParameter('name', '%'.$city.'%')
 		->setMaxResults($limit)
 		->addOrderBy('a.id');
 		
 		$cities = $query->getQuery()->getArrayResult();
+
+		// Función que compara los elementos segun su coincidencía
+		function sorter($city) {
+			
+			return function ($a, $b) use ($city) {
+				if(is_array($a)){
+					similar_text($a['nameUtf8'],$city,$a_percent);
+					similar_text($b['nameUtf8'],$city,$b_percent);
+				}
+				else{
+					similar_text($a->getNameUtf8(),$city,$a_percent);
+					similar_text($b->getNameUtf8(),$city,$b_percent);
+				}
+				if($a_percent==$b_percent){
+					return 0;
+				}
+				else if($a_percent<$b_percent){
+					return 1;
+				}
+				else{
+					return -1;
+				}
+			};
+		}
+		usort($cities, sorter($city));
 		
-		// TODO: Ordenar de mayor a menor coincidencia
-		
+		//En caso de querer devolver un array de objetos lo construye 
 		if ($objects) {
 			$cities_old = $cities;
 			$cities = array();
@@ -44,7 +71,6 @@ class Helper {
 				$cities[] = $reJJ->findOneById($c['id']);
 			}
 		}
-		
 		return $cities;		
 	}
 }
