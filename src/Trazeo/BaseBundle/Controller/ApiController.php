f748cc255de38b2c94342ea588874bfcf9d6cf59
sopinet <sopinet@sopinet-desktop.(none)>
2014-06-04 09:11:04 +0200

    Children are now free from ride at the end of this

--- src/Trazeo/BaseBundle/Controller/ApiController.php
+++ src/Trazeo/BaseBundle/Controller/ApiController.php
@@ -81,7 +81,7 @@ class ApiController extends Controller {
 	 * Funcion que controla si el usuario está logueado o se comprueba con su email y pass
 	 */
 	private function checkPrivateAccess(Request $request) {
-		//$user = $this->get('security.context')->getToken()->getUser();
+		$user = $this->get('security.context')->getToken()->getUser();
 	
 		/*if ($user != null && $user != "anon.") {
 			return $user;
@@ -92,12 +92,12 @@ class ApiController extends Controller {
 // 			return false;
 // 		}
 	
-		$user = $this->checkUser($request->get('email'), $request->get('pass'));
+		/*$user = $this->checkUser($request->get('email'), $request->get('pass'));
 	
 		//No es necesario
 		if($user == false) {
 			return false;
-		}
+		}*/
 	
 		return $user;
 	}
@@ -237,6 +237,16 @@ class ApiController extends Controller {
 					
 					
 					$group->setRide(null);
+					
+					//desvinculamos a los niños del paseo 
+					$childs = $em->getRepository('TrazeoBaseBundle:EChild')->findByRide($ride);
+					foreach ($childs as $child){
+						$child->setRide(null);
+						$em->persist($child);
+						
+					}
+					$em->flush();
+					
 				}
 				
 				$ride = new ERide();
@@ -565,6 +575,15 @@ class ApiController extends Controller {
 		$ride->setGroupid($group->getId());
 		$ride->setGroup(null);
 		$em->persist($ride);
+		
+		//desvinculamos a los niños del paseo
+		$childs = $em->getRepository('TrazeoBaseBundle:EChild')->findByRide($ride);
+		foreach ($childs as $child){
+			$child->setRide(null);
+			$em->persist($child);
+		
+		}
+		$em->flush();
 		
 		$event = new EEvent();
 		$event->setRide($ride);
