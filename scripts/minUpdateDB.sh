e757edd11eaa8244fe29ba0a99f1d1949188b0ce
sopinet <sopinet@sopinet-desktop.(none)>
2014-07-02 13:31:32 +0200

    script update for database

--- scripts/minUpdateDB.sh
+++ scripts/minUpdateDB.sh
@@ -3,6 +3,7 @@ if [ -f "composer.json" ];
 then
 	php app/console doctrine:generate:entities TrazeoBaseBundle
 	php app/console doctrine:schema:update --force
+	php app/console doctrine:schema:update --force --env=prod
 else
 	echo "Error. Sitúese en el directorio raíz de Symfony";
 fi
