a7cb9e915dc4fa1d91e5b081293320a6ea0df1b5
alejandro <alejandro.arrabal.hidalgo1986@gmail.com>
2014-10-03 09:47:17 +0200

    Hotfixes

--- src/Trazeo/BaseBundle/Form/UserExtendType.php
+++ src/Trazeo/BaseBundle/Form/UserExtendType.php
@@ -35,7 +35,7 @@ class UserExtendType extends AbstractType
                     'required' => false
                     )
                 )
-             ->add('country', 'hidden', array(
+             ->add('country', 'entity', array(
             		'class' => 'JJsGeonamesBundle:Country',
             		'attr' => array('class' => 'chosen-select'),
              		'required' => false, //Para permitir hacer submit con una opción 'vacía'
