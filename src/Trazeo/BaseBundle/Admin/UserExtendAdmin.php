5d17ad2d30418b7f53d662e20b784a3961231484
Alejandro <alejandro.arrabal.hidalgo1986@gmail.com>
2014-11-07 09:30:13 +0100

    Admin dashboard

--- src/Trazeo/BaseBundle/Admin/UserExtendAdmin.php
+++ src/Trazeo/BaseBundle/Admin/UserExtendAdmin.php
@@ -1,92 +1,76 @@
 <?php
 namespace Trazeo\BaseBundle\Admin;
- 
+
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
- 
+use Sonata\AdminBundle\Show\ShowMapper;
+
 class UserExtendAdmin extends Admin
 {
-  protected $translationDomain = 'TrazeoBaseBundleAdmin';
+    protected $translationDomain = 'TrazeoBaseBundleAdmin';
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('nick')
+            ->add('points')
+            ->add('spendedPoints')
+            ->add('mobile')
+        ;
+    }
 
-  protected function configureFormFields(FormMapper $formMapper)
-  {
-    $formMapper
-        ->add('user')
-        ->add('groups', null, array('required' => false))
-        ->add('adminRoutes', null, array('required' => false))
-        ->add('childs', null, array('required' => false))
-        ->add('nick')
-        ->add('tutorial', null, array('required' => false))
-        ->add('tutorialMap', null, array('required' => false))
-        ->add('points')
-        ->add('spendedPoints')
-        ->add('mobile')
-        ->add('city.nameUtf8')
-        ->add('useLike')
-    ;
-  }
- 
-  protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-  {
-    $datagridMapper
-    	->add('nick')
-    	->add('groups')
-    	->add('tutorial')
-    	->add('tutorialMap')    	
-      ->add('points')
-      ->add('spendedPoints')
-      ->add('mobile')
-      ->add('city.nameUtf8')
-    	->add('useLike')
-    ;
-  }
- 
-  protected function configureListFields(ListMapper $listMapper)
-  {
-    $listMapper
-      ->addIdentifier('id')
-      ->add('user')
-      ->add('groups')
-      ->add('adminRoutes')
-      ->add('childs')
-      ->add('nick')      
-      ->add('useLike')
-      ->add('points')
-      ->add('spendedPoints')
-      ->add('mobile')
-      ->add('city.nameUtf8')
-    ;
-  }
- 
-/*     protected function configureShowFields(ShowMapper $showMapper)
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('nick')
+            ->add('groups')
+            ->add('tutorial')
+            ->add('tutorialMap')
+            ->add('points')
+            ->add('spendedPoints')
+            ->add('mobile')
+            ->add('city.nameUtf8')
+            ->add('useLike')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('nick')
+            ->add('points')
+            ->add('spendedPoints')
+            ->add('mobile')
+            ->add('city.nameUtf8')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+    protected function configureShowFields(ShowMapper $showMapper)
     {
         // Here we set the fields of the ShowMapper variable, $showMapper (but this can be called anything)
         $showMapper
-      ->addIdentifier('id')
-      ->add('user')
-      ->add('groups')
-      ->add('adminRoutes')
-      ->add('childs')
-      ->add('nick')      
-      ->add('useLike')
-      ->add('points')
-      ->add('spendedPoints')
-      ->add('mobile')
-      ->add('city.nameUtf8')
-      ;
-
-    }*/
-
-  public function validate(ErrorElement $errorElement, $object)
-  {
-    /*$errorElement
-      ->with('text')
-      ->assertMaxLength(array('limit' => 3))
-      ->end()
-    ;*/
-  }
+            ->add('nick')
+            ->add('points')
+            ->add('spendedPoints')
+            ->add('mobile')
+        ;
+    }
+    public function validate(ErrorElement $errorElement, $object)
+    {
+        /*$errorElement
+          ->with('text')
+          ->assertMaxLength(array('limit' => 3))
+          ->end()
+        ;*/
+    }
 }
 ?>
