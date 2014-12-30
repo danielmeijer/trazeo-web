de3f37b52aa38aa8f57f7c9ab1cc23ed3c88f514
Fernando Hidalgo <fhidalgo@sopinet.com>
2014-12-30 01:22:51 +0100

    Added direct form layout

--- src/Trazeo/MyPageBundle/Controller/FrontController.php
+++ src/Trazeo/MyPageBundle/Controller/FrontController.php
@@ -12,17 +12,22 @@ use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\ViewHandler;
 use FOS\RestBundle\Controller\FOSRestController;
+use Trazeo\BaseBundle\Entity\EChild;
+use Trazeo\BaseBundle\Form\ChildType;
+use Trazeo\BaseBundle\Form\UserType;
 use Trazeo\MyPageBundle\Classes\Module\TrazeoGroups;
+use Application\Sonata\UserBundle\Entity\User;
+use Trazeo\MyPageBundle\Form\UserDirectType;
 
 /**
-* @Route("/landingPage")
+* @Route("/front")
 */
 class FrontController extends Controller
 {
-/**
-* @Route("/{subdomain}", name="landingPage")
-* @Template()
-*/
+    /**
+    * @Route("/landing/{subdomain}", name="landingPage")
+    * @Template()
+    */
    public function landingPageAction($subdomain)
    {
 	   	$em = $this->getDoctrine()->getEntityManager();
@@ -35,4 +40,45 @@ class FrontController extends Controller
             'page' => $page
 	   	);
 	}
+
+    /**
+     * @Route("/inGroup/{group_id}", name="loginInGroup")
+     * @Template()
+     */
+    public function loginInGroupAction($group_id) {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $repositoryGroup = $em->getRepository("TrazeoBaseBundle:EGroup");
+        $group = $repositoryGroup->findOneById($group_id);
+
+        $user = new User();
+        $form_user = $this->createForm(new UserDirectType(), $user);
+
+        $child = new EChild();
+        $form_children = $this->createForm(new ChildType(), $child, array(
+            'action' => $this->generateUrl('panel_child_create'),
+            'method' => 'POST',
+            'attr' => array(
+                'Children.help.nick' => $this->get('translator')->trans('Children.help.nick'),
+                'Children.help.datebirth' => $this->get('translator')->trans('Children.help.datebirth'),
+                'Children.help.visibility' => $this->get('translator')->trans('Children.help.visibility'),
+                'Children.help.gender' => $this->get('translator')->trans('Children.help.gender'),
+                'Children.help.scholl' => $this->get('translator')->trans('Children.help.scholl'),
+            )
+        ));
+
+        return array(
+            'group' => $group,
+            'form_user' => $form_user->createView(),
+            'form_children' => $form_children->createView()
+
+        );
+    }
+
+    /**
+     * @Route("saveInGroup/{group_id}", name="saveInGroup")
+     */
+    public function saveInGroup() {
+        $em = $this->getDoctrine()->getEntityManager();
+    }
 }
---
+++ src/Trazeo/MyPageBundle/Form/UserDirectType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Trazeo\MyPageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserDirectType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', 'email', array(
+                'attr' => array(
+                    'placeholder' => 'Register.email',
+                    'data-content' => 'Register.email'
+                ),'required' => true))
+            ->add('password', 'repeated', array(
+                'type' => 'password',
+                'invalid_message' => 'Ambas contraseñas deben coincidir',
+                'required' => false,
+                'options' => array('attr' => array('class' => 'password-field')),
+                'first_options' => array('attr' => array('placeholder' => 'Form.profile.pass',        					'data-toggle' => 'popover',
+                    'data-placement' => 'right',
+                    'data-content' => 'Form.profile.pass')),
+                'second_options' => array ('attr' => array('placeholder' => 'Form.profile.repeatpass',        					'data-toggle' => 'popover',
+                    'data-placement' => 'right',
+                    'data-content' => 'Form.profile.repeatpass')),
+
+            ))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Application\Sonata\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'trazeo_mypagebundle_userdirecttype';
+    }
+}
