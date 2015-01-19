a8380ca8d5ef296287c709f94a938dfce3f69243
Fernando Hidalgo <fhidalgo@sopinet.com>
2015-01-19 18:10:09 +0100

    fixed

--- web/app_dev.php
+++ web/app_dev.php
@@ -19,7 +19,7 @@ use Symfony\Component\Debug\Debug;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
-ini_set("display_errors",1);
+
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
