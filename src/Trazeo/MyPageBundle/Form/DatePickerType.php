ffad8bdde2912cbb56317682d6a46dc860c1f02a
Fernando Hidalgo <fhidalgo@sopinet.com>
2015-02-18 10:22:25 +0100

    Added fixed bugs in private-panel

---
+++ src/Trazeo/MyPageBundle/Form/DatePickerType.php
@@ -0,0 +1,35 @@
+<?php
+namespace Trazeo\MyPageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DatePickerType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('Inicio', $options['field_type'], array_merge(array('required' => false), $options['field_options']));
+        //$builder->add('Fin', $options['field_type'], array_merge(array('required' => false), $options['field_options']));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'field_options'    => array(),
+            'field_type'       => 'sonata_type_date_picker',
+        ));
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return "DatePicker";
+    }
+}
