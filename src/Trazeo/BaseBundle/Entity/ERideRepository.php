a70757d3e2d5f0a6e6188392c91cb755bb67f126
Fernando Hidalgo <fhidalgo@sopinet.com>
2015-02-23 22:12:16 +0100

    Included edit option for ERide

--- src/Trazeo/BaseBundle/Entity/ERideRepository.php
+++ src/Trazeo/BaseBundle/Entity/ERideRepository.php
@@ -5,6 +5,12 @@ use Doctrine\ORM\EntityRepository;
 
 class ERideRepository extends EntityRepository
 {
+    /**
+     * Devuelve listado de objetos Niño en el Paseo
+     *
+     * @param ERide $ride
+     * @return array
+     */
     public function getChildrenInRide(ERide $ride) {
         // Si no tenemos niños, se revisa el paseo
         $em = $this->getEntityManager();
@@ -13,5 +19,17 @@ class ERideRepository extends EntityRepository
         $childsInRide = $repositoryEChildRide->findBy(array('ride' => $ride->getId()));
 
         return $childsInRide;
+    }
+
+    /**
+     * Devuelve el numero de participaciones en un Paseo
+     * Comprueba previamente el campo FixChildCount que es usado para corregir errores
+     *
+     * @param ERide $ride
+     * @return int
+     */
+    public function getFixCountChildrenInRide(ERide $ride) {
+        if ($ride->getFixChildCount() != null) return $ride->getFixChildCount();
+        return count($this->getChildrenInRide($ride));
     }
 }
