13153cc992dd7306ace67acb6970d25007c725ff
alejandro <aarrabal@sopinet.com>
2016-01-22 08:34:33 +0100

    Added datalist for school on groups

---
+++ src/Trazeo/BaseBundle/Form/Type/DatalistType.php
@@ -0,0 +1,31 @@
+<?php
+// src/Acme/Form/Type/DatalistType
+namespace Trazeo\BaseBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DatalistType extends AbstractType
+{
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setRequired(['choices']);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['choices']= $options['choices'];
+    }
+
+    public function getName()
+    {
+        return 'datalist';
+    }
+}
