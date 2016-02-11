78a593dffb5ff07c9181ca869ed45a24b22d9e6a
jmartos89 <jmartos89@gmail.com>
2016-02-11 12:43:17 +0100

    Create php-background.sh
    Add script to rabbitmqqueue

---
+++ scripts/php-background/php-background.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#Variables auxiliares
+PIDS="/var/www/vhosts/ns3000631.ovh.net/beta.trazeo.es/trazeo-web/scripts/php-background/pids.txt";
+PROCESSES="/var/www/vhosts/ns3000631.ovh.net/beta.trazeo.es/trazeo-web/scripts/php-background/processes.txt";
+
+touch $PIDS;
+
+#Almacenamos los PIDs de los procesos iniciados en php
+echo "$(pgrep php)" > $PIDS;
+
+while read process
+do
+
+ find=0;
+
+ while read pid
+ do
+
+  if [ "$pid" != "" ]; then
+  echo  "Comparamos proceso" $process "con pid: " $pid;
+  var1="$(cat /proc/$pid/cmdline)";
+  echo "Variable 1" $var1;
+  var2="${process//[[:space:]]/}";
+  echo -n "Variable 2" $var2;
+
+  if [ "$var1" == "$var2"  ]; then
+   find=1;
+   echo -e "Proceso" $process  "ejecutandose \n";
+   break;
+  fi 
+
+  fi
+
+ done < $PIDS;
+
+ if [ "$find" == 0  ]; then
+  echo "Ejecutamos el proceso " $process;
+  eval "$process &";
+ fi
+
+done < $PROCESSES; 
